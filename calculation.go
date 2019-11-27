e4ef88079fbea9af07d3c6373aa434b0950a4b16
a2210 <aakash@careersocially.com>
2019-11-27 22:10:39 +0530

    first commit

---
+++ calculation.go
@@ -0,0 +1,24 @@
+package factorial
+
+import "fmt"
+
+func Factorial(n int64) {
+	var val, i int64
+	val = 1
+
+
+	if n == 0 {
+		fmt.Println("factorial of ", n, "is = ", 1)
+	}else {
+		for i = 1; i <= n; i++ {
+			val = val*i
+
+		}
+
+		fmt.Println("factorial of ", n, "is = ", val)
+
+
+	}
+}
+
+
